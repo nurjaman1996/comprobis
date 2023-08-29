f088924bd1cc673394aada28fcf51b6a5e4a1683
nurjaman1996 <nurjamanmuhammad@gmail.com>
2023-08-30 02:36:22 +0700

    asd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
